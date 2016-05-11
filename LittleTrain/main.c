f317fa78336969bc8e26193d6ce9fc0aa4bf9c0a
LinkinYoung <1143052146@qq.com>
2016-05-11 14:36:35 +0800

    Initial Commit

---
+++ LittleTrain/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LittleTrain
+//
+//  Created by 杨林青 on 16/5/11.
+//  Copyright © 2016年 1601. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ LittleTrain/train.h
@@ -0,0 +1,13 @@
+//
+//  train.h
+//  LittleTrain
+//
+//  Created by 杨林青 on 16/5/11.
+//  Copyright © 2016年 1601. All rights reserved.
+//
+
+#ifndef train_h
+#define train_h
+
+
+#endif /* train_h */
