57e28c37dca10e5846165f7efa41a7cbcf49dcd9
LinkinYoung <1143052146@qq.com>
2016-06-13 18:14:45 +0800

    启动画面测试通过 原来的定义有很多冲突要改

--- TrainGUI/TrainGUI/TrainGUI.cpp
+++ TrainGUI/TrainGUI/TrainGUI.cpp
@@ -7,14 +7,13 @@
 int windowWidth = 960;
 int windowHeight = 560;
 
-void cutBoard(HWND window);
+void cutBoard(HWND window);//将程序窗口剪裁至仅有客户区
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-
 	initgraph(windowWidth, windowHeight);
 	// 获取窗口句柄
 	HWND hWnd = GetHWnd();
@@ -22,7 +21,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	IMAGE APP_Start;
 	loadimage(&APP_Start, _T("./Res/APP_START.jpg"));
 	putimage(0, 0, &APP_Start);
-	MOUSEMSG m;
+	Sleep(500);//等待0.5秒
+	fillrectangle(0, 0, 400, 400);//
+	MOUSEMSG m;//等待鼠标点击后退出程序
 	while (1)
 	{
 		m = GetMouseMsg();
