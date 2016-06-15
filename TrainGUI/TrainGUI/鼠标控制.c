9d87578f5366c584d745e9ae5dc1e3240fbe9a94
Shine101099 <791195819@qq.com>
2016-06-15 10:25:46 +0800

    语法修改
    语法修改

--- TrainGUI/TrainGUI/鼠标控制.c
+++ TrainGUI/TrainGUI/鼠标控制.c
@@ -3,8 +3,8 @@
 #include<tchar.h>
 #include<train.h>
 #include<TrainGUI.h>
-MOUSEMSG m,n;		// 定义鼠标消息
-
+#include<stdlib.h>
+MOUSEMSG m;		// 定义鼠标消息
 int i,k;
 int servertrain=0;
 int stopTime;
@@ -45,18 +45,19 @@ int pointnodeid(MOUSEMSG m)
 	else
 	return -1;
 }
-	// 获取一条鼠标消息
+unsigned __stdcall GUIInput(void* pArguments)
+{// 获取一条鼠标消息
 	m = GetMouseMsg();
 
 	if (m.uMsg == WM_LBUTTONDOWN)
 	{
 		if (programStat == 1)
 		{
-			if (n.x >= 347 && n.x <= 478 && n.y >= 302 && n.y <= 330)
+			if (m.x >= 347 && m.x <= 478 && m.y >= 302 && m.y <= 330)
 			{
 				programStat = 0;
 			}
-			else if (n.x >= 347 && n.x <= 521 && n.y >= 258 && n.x <= 280)
+			else if (m.x >= 347 && m.x <= 521 && m.y >= 258 && m.x <= 280)
 			{//立即结束整个工程？
 			}
 		}
@@ -75,15 +76,15 @@ int pointnodeid(MOUSEMSG m)
 				{
 					if (trackNodeList[k]->type == STATION)
 					{
-					 currentmode = S;
-					 stationnumber = k;
-					 //右侧贴站点的模块图(STATION_MODE.jpg);？
+						currentmode = S;
+						stationnumber = k;
+						//右侧贴站点的模块图(STATION_MODE.jpg);？
 					}
 					else if (trackNodeList[k]->type == BRANCH)
 					{
-							currentmode = P;
-						   branchnumber = k;
-						   //右侧贴公共轨道的模块图(BRANCH_MODE.jpg);？
+						currentmode = P;
+						branchnumber = k;
+						//右侧贴公共轨道的模块图(BRANCH_MODE.jpg);？
 					}
 
 				}
@@ -94,261 +95,262 @@ case(m.x >= 916 && m.x <= 960 && m.y >= 0 && m.y <= 44) :
 	mainData = calloc(1, sizeof(struct _mainQueueNode));    //点击关闭
 	mainData->type = MLOCK;
 	append(mainMission, mainData);
-	toexit = 1;                              //表示应该弹出退出窗口,结束线程？
+	programStat == 1;                              //表示应该弹出退出窗口,结束线程？
 	break;
 
-case(m.x >= 711 && m.x <= 740 && m.y >= 473 && m.y <= 502):
-		if (servicePolicy == BYTHEWAY)
-		{
-			servicePolicy = SEQUENCING;
-			RECT r = { 751, 478, 848, 498 };
-			drawtext(_T("顺序服务"), &r);
-		}
-		else
-		{
-			servicePolicy = BYTHEWAY;
-			RECT r = { 751, 478, 848, 498 };
-			drawtext(_T("顺便服务"), &r);
-		}
-	break;
+case(m.x >= 711 && m.x <= 740 && m.y >= 473 && m.y <= 502) :
+	if (servicePolicy == BYTHEWAY)
+	{
+		servicePolicy = SEQUENCING;
+		RECT r = { 751, 478, 848, 498 };
+		drawtext(_T("顺序服务"), &r);
+	}
+	else
+	{
+		servicePolicy = BYTHEWAY;
+		RECT r = { 751, 478, 848, 498 };
+		drawtext(_T("顺便服务"), &r);
+	}
+														   break;
 
 case(m.x >= 862 && m.x <= 891 && m.y >= 473 && m.y <= 502) :
-		if (servicePolicy == BYTHEWAY)
-		{
-			servicePolicy = SEQUENCING;
-			RECT r = { 751, 478, 848, 498 };
-			drawtext(_T("顺序服务"), &r);
-		}
-		else
-		{
-			servicePolicy = BYTHEWAY;
-			RECT r = { 751, 478, 848, 498 };
-			drawtext(_T("顺便服务"), &r);
-		}
-															   break;
+	if (servicePolicy == BYTHEWAY)
+	{
+		servicePolicy = SEQUENCING;
+		RECT r = { 751, 478, 848, 498 };
+		drawtext(_T("顺序服务"), &r);
+	}
+	else
+	{
+		servicePolicy = BYTHEWAY;
+		RECT r = { 751, 478, 848, 498 };
+		drawtext(_T("顺便服务"), &r);
+	}
+														   break;
 
 case(m.x >= 711 && m.x <= 740 && m.y >= 511 && m.y <= 540) :
-		if (controlPolicy == AUTO)
-		{
-			controlPolicy = MANUAL;
-				RECT r = { 751, 516, 848, 536 };
-			drawtext(_T("人工"), &r);
-		}
-		else
-		{
-			controlPolicy = AUTO;
-				RECT r = { 751, 516, 848, 536 };
-			drawtext(_T("自动"), &r);
-		}
-															   break;
+	if (controlPolicy == AUTO)
+	{
+		controlPolicy = MANUAL;
+		RECT r = { 751, 516, 848, 536 };
+		drawtext(_T("人工"), &r);
+	}
+	else
+	{
+		controlPolicy = AUTO;
+		RECT r = { 751, 516, 848, 536 };
+		drawtext(_T("自动"), &r);
+	}
+														   break;
 
-	case(m.x >= 862 && m.x <= 891 && m.y >= 511 && m.y <= 540) :
-		if (controlPolicy == AUTO)
-		{
-			controlPolicy = MANUAL;
-				RECT r = { 751, 516, 848, 536 };
-			drawtext(_T("人工"), &r);
-		}
-		else
-		{
-			controlPolicy = AUTO;
-				RECT r = { 751, 516, 848, 536 };
-			drawtext(_T("自动"), &r);
-		}
-															   break;
+case(m.x >= 862 && m.x <= 891 && m.y >= 511 && m.y <= 540) :
+	if (controlPolicy == AUTO)
+	{
+		controlPolicy = MANUAL;
+		RECT r = { 751, 516, 848, 536 };
+		drawtext(_T("人工"), &r);
+	}
+	else
+	{
+		controlPolicy = AUTO;
+		RECT r = { 751, 516, 848, 536 };
+		drawtext(_T("自动"), &r);
+	}
+														   break;
 
-	case(m.x >= 709 && m.x <= 819 && m.y >= 68 && m.y <= 94): //全部开始
-		if (currentmode == T)
-		{
-			for (i = 0; i < totaltrain; i++)
-			{
-				mainData = (mainQueueNode)calloc(1, sizeof(struct _mainQueueNode));
-				mainData->type = MRESUME;
-				mainData->train.id = i;
-				append(mainMission, mainData);
-			}
-		}
-		break;
-
-
-	case(m.x >= && m.x <=  && m.y >= && m.y <):  //全部暂停?按钮
-		if (currentmode == T)
-		{
-			for (i = 0; i < totaltrain; i++)
-			{
-				mainData = (mainQueueNode)calloc(1, sizeof(struct _mainQueueNode));
-				mainData->type = MPAUSE;
-				mainData->train.id = i;
-				append(mainMission, mainData);
-			}
-		}
-		break;
-		
-	case(m.x >= 709 && m.x <= 738 && m.y >= 107 & m.y <= 136):
-		if (currentmode == T)
-		{
-			if (trainnumber > 0)
-			{
-				trainnumber--;
-				TCHAR tid[20];
-				_stprintf(tid, _T("%d"),trainnumber);
-				RECT r = { 785, 107, 814, 136 };
-				drawtext(tid, &r);
-			}
-		}
-		break;
-
-		case(m.x >= 860 && m.x <= 889 && m.y >= 107 && m.y <= 136):
-		if (currentmode == T)
-		{
-			if (trainnumber <totaltrain - 1)
-			{
-				trainnumber++;
-				TCHAR tid[20];
-				_stprintf(tid, _T("%d",trainnumber);
-				RECT r = { 785, 107, 814, 136 };
-				drawtext(tid, &r);
-			}
-		}
-		break;
-
-	case(m.x >= 711 && m.x <= 801 && m.y >= 153 && m.y <= 178) : //暂停这辆车
-		if (currentmode == T)
-		{
-			mainData = (mainQueueNode)calloc(1, sizeof(struct _mainQueueNode));
-			mainData->type = MPAUSE;
-			mainData->train.id = trainnumber;
-			append(mainMission, mainData);
-	     }
-															   break;
-
-	case(m.x >=  && m.x <=  && m.y >=  && m.y <= ) : //开始这辆车？按钮
-		if (currentmode == T)
+case(m.x >= 709 && m.x <= 819 && m.y >= 68 && m.y <= 94) : //全部开始
+	if (currentmode == T)
+	{
+		for (i = 0; i < totaltrain; i++)
 		{
 			mainData = (mainQueueNode)calloc(1, sizeof(struct _mainQueueNode));
 			mainData->type = MRESUME;
-			mainData->train.id = trainnumber;
+			mainData->train.id = i;
 			append(mainMission, mainData);
 		}
-															   break;
+	}
+														   break;
 
-	case(m.x >= 748 && m.x <= 777 && m.y >= 191 && m.y <= 220) :
-		if (currentmode == T)
-		{
-			if (trainList[trainnumber]->v > 0)
-			{
-				trainList[trainnumber]->v--;
-				TCHAR tid[20];
-				_stprintf(tid, _T("%dm/s"),trainList[trainnumber]->v);
-				RECT r = { 787, 195, 891, 216 };
-				drawtext(tid, &r);
-			}
-		}
-															   break;
 
-	case(m.x >= 899 && m.x <= 928 && m.y >= 191 && m.y <= 220) :
-		if (currentmode == T)
+case(m.x >= && m.x <= && m.y >= && m.y < ) :  //全部暂停?按钮
+	if (currentmode == T)
+	{
+		for (i = 0; i < totaltrain; i++)
 		{
-			if (trainList[trainnumber]->v < 10)
-			{
-				trainList[trainnumber]->v++;
-				TCHAR tid[20];
-				_stprintf(tid, _T("%dm/s"),trainList[trainnumber]->v);
-				RECT r = { 787, 195, 891, 216 };
-				drawtext(tid, &r);
-			}
+			mainData = (mainQueueNode)calloc(1, sizeof(struct _mainQueueNode));
+			mainData->type = MPAUSE;
+			mainData->train.id = i;
+			append(mainMission, mainData);
 		}
-															   break;
+	}
+											  break;
 
-	case(m.x >= 748 && m.x <= 777 && m.y >= 224 && m.y <= 253) :
-		if (currentmode == T)
+case(m.x >= 709 && m.x <= 738 && m.y >= 107 & m.y <= 136) :
+	if (currentmode == T)
+	{
+		if (trainnumber > 0)
 		{
-			if (trainList[trainnumber]->direction == clockwise)
-			{
-				trainList[trainnumber]->direction = anticlockwise;
-				RECT r = { 785, 228, 889, 249 };
-				drawtext(_T("ANTICLOCKWISE"), &r);
-			}
-			else
-			{
-				trainList[trainnumber]->direction = clockwise;
-				RECT r = { 785, 228, 889, 249 };
-				drawtext(_T("CLOCKWISE"), &r);
-			}
+			trainnumber--;
+			TCHAR tid[20];
+			_stprintf(tid, _T("%d"), trainnumber);
+			RECT r = { 785, 107, 814, 136 };
+			drawtext(tid, &r);
 		}
-															   break;
+	}
+														  break;
 
-	case(m.x >= 899 && m.x <= 928 && m.y >= 224 && m.y <= 253) :
-		if (currentmode == T)
+case(m.x >= 860 && m.x <= 889 && m.y >= 107 && m.y <= 136) :
+	if (currentmode == T)
+	{
+		if (trainnumber < totaltrain - 1)
 		{
-			if (trainList[trainnumber]->direction == clockwise)
-			{
-				trainList[trainnumber]->direction = anticlockwise;
-				RECT r = { 785, 228, 889, 249 };
-				drawtext(_T("ANTICLOCKWISE"), &r);
-			}
-			else
-			{
-				trainList[trainnumber]->direction = clockwise;
-				RECT r = { 785, 228, 889, 249 };
-				drawtext(_T("CLOCKWISE"), &r);
-			}
+			trainnumber++;
+			TCHAR tid[20];
+			_stprintf(tid, _T("%d", trainnumber);
+			RECT r = { 785, 107, 814, 136 };
+			drawtext(tid, &r);
 		}
-															   break;
+	}
+														   break;
 
-	case(m.x >= 709 && m.x <= 779 && m.y >= 71 && m.y <= 93) :
-		if (currentmode == S || currentmode == P)
-		{
-			currentmode = T;
-			//贴小火车模式图(TRAIN_MODE.jpg)？							
-		}
+case(m.x >= 711 && m.x <= 801 && m.y >= 153 && m.y <= 178) : //暂停这辆车
+	if (currentmode == T)
+	{
+		mainData = (mainQueueNode)calloc(1, sizeof(struct _mainQueueNode));
+		mainData->type = MPAUSE;
+		mainData->train.id = trainnumber;
+		append(mainMission, mainData);
+	}
 															 break;
 
-	case(m.x >= 711 && m.x <= 739 && m.y >= 349 && m.y <= 378) :
-		if (currentmode == S)
+case(m.x >= && m.x <= && m.y >= && m.y <= ) : //开始这辆车？按钮
+	if (currentmode == T)
+	{
+		mainData = (mainQueueNode)calloc(1, sizeof(struct _mainQueueNode));
+		mainData->type = MRESUME;
+		mainData->train.id = trainnumber;
+		append(mainMission, mainData);
+	}
+											  break;
+
+case(m.x >= 748 && m.x <= 777 && m.y >= 191 && m.y <= 220) :
+	if (currentmode == T)
+	{
+		if (trainList[trainnumber]->v > 0)
 		{
-			if (servetrain > 0)
-			{
-				servertrain--;
-				TCHAR tid[20];
-				_stprintf(tid, _T("%d"),servertrain);
-				RECT r = { 784, 356, 801, 374 };
-				drawtext(tid, &r);
-			}
+			trainList[trainnumber]->v--;
+			TCHAR tid[20];
+			_stprintf(tid, _T("%dm/s"), trainList[trainnumber]->v);
+			RECT r = { 787, 195, 891, 216 };
+			drawtext(tid, &r);
 		}
-															   break;
+	}
+														   break;
+
+case(m.x >= 899 && m.x <= 928 && m.y >= 191 && m.y <= 220) :
+	if (currentmode == T)
+	{
+		if (trainList[trainnumber]->v < 10)
+		{
+			trainList[trainnumber]->v++;
+			TCHAR tid[20];
+			_stprintf(tid, _T("%dm/s"), trainList[trainnumber]->v);
+			RECT r = { 787, 195, 891, 216 };
+			drawtext(tid, &r);
+		}
+	}
+														   break;
+
+case(m.x >= 748 && m.x <= 777 && m.y >= 224 && m.y <= 253) :
+	if (currentmode == T)
+	{
+		if (trainList[trainnumber]->direction == clockwise)
+		{
+			trainList[trainnumber]->direction = anticlockwise;
+			RECT r = { 785, 228, 889, 249 };
+			drawtext(_T("ANTICLOCKWISE"), &r);
+		}
+		else
+		{
+			trainList[trainnumber]->direction = clockwise;
+			RECT r = { 785, 228, 889, 249 };
+			drawtext(_T("CLOCKWISE"), &r);
+		}
+	}
+														   break;
+
+case(m.x >= 899 && m.x <= 928 && m.y >= 224 && m.y <= 253) :
+	if (currentmode == T)
+	{
+		if (trainList[trainnumber]->direction == clockwise)
+		{
+			trainList[trainnumber]->direction = anticlockwise;
+			RECT r = { 785, 228, 889, 249 };
+			drawtext(_T("ANTICLOCKWISE"), &r);
+		}
+		else
+		{
+			trainList[trainnumber]->direction = clockwise;
+			RECT r = { 785, 228, 889, 249 };
+			drawtext(_T("CLOCKWISE"), &r);
+		}
+	}
+														   break;
+
+case(m.x >= 709 && m.x <= 779 && m.y >= 71 && m.y <= 93) :
+	if (currentmode == S || currentmode == P)
+	{
+		currentmode = T;
+		//贴小火车模式图(TRAIN_MODE.jpg)？							
+	}
+														 break;
+
+case(m.x >= 711 && m.x <= 739 && m.y >= 349 && m.y <= 378) :
+	if (currentmode == S)
+	{
+		if (servertrain > 0)
+		{
+			servertrain--;
+			TCHAR tid[20];
+			_stprintf(tid, _T("%d"), servertrain);
+			RECT r = { 784, 356, 801, 374 };
+			drawtext(tid, &r);
+		}
+	}
+														   break;
 
 
-	case(m.x >= 862 && m.x <= 890 && m.y >= 349 && m.y <= 378) :
-		if (currentmode == S)
+case(m.x >= 862 && m.x <= 890 && m.y >= 349 && m.y <= 378) :
+	if (currentmode == S)
+	{
+		if (servertrain < totaltrain - 1)
 		{
-			if (servetrain < totaltrain - 1)
-			{
-				servertrain++;
-				TCHAR tid[20];
-				_stprintf(tid, _T("%d"),servertrain);
-				RECT r = { 784, 356, 801, 374 };
-				drawtext(tid, &r);
-			}
+			servertrain++;
+			TCHAR tid[20];
+			_stprintf(tid, _T("%d"), servertrain);
+			RECT r = { 784, 356, 801, 374 };
+			drawtext(tid, &r);
 		}
-															   break;
+	}
+														   break;
 
-	case(m.x >= 712 && m.x <= 822 && m.y >= 381 && m.y <= 404) :
-		if (currentmode == S)
+case(m.x >= 712 && m.x <= 822 && m.y >= 381 && m.y <= 404) :
+	if (currentmode == S)
+	{
+		if (cangivemission(stationnumber, servertrain) == 1)
 		{
-			if (cangivemission(stationnumber, servertrain) == 1)
-			{
-				stopTime = (rand() % 9) + 2;//(rand() % (10-2+1))+ a;
-				mainData = calloc(1, sizeof(struct _mainQueueNode));
-				mainData->type = MSTATION;
-				mainData->station.id = stationnumber;
-				mainData->station.time = stopTime;
-				mainData->station.train = servertrain;
-				append(mainMission, mainData);
-			}
+			stopTime = (rand() % 9) + 2;//(rand() % (10-2+1))+ a;
+			mainData = calloc(1, sizeof(struct _mainQueueNode));
+			mainData->type = MSTATION;
+			mainData->station.id = stationnumber;
+			mainData->station.time = stopTime;
+			mainData->station.train = servertrain;
+			append(mainMission, mainData);
 		}
-															   break;
+	}
+														   break;
 
 			}
 		}
 	}
+}
